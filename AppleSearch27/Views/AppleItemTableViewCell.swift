f0d7d15653ed426e3d7069cab0dac321c40d3606
Drew Seeholzer <drews21f7@gmail.com>
2019-06-27 10:30:48 -0600

    Layout views

---
+++ AppleSearch27/Views/AppleItemTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AppleItemTableViewCell.swift
+//  AppleSearch27
+//
+//  Created by Drew Seeholzer on 6/27/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import UIKit
+
+class AppleItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var trackLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+}
